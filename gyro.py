281a8b91397ec18a019f4ea6e7b81ef9ae5fd80d
Yash Indane <yashindane46@gmail.com>
2021-01-15 23:35:28 +0530

    Update gyro.py

--- gyro.py
+++ gyro.py
@@ -1,11 +1,12 @@
 # importing socket module 
+
 import socket
 import binascii
 import pyautogui
 import joblib
 
-UDP_IP = ''
-UDP_PORT = 
+UDP_IP = '192.168.29.176'
+UDP_PORT = 5600
 model = joblib.load('svm_classifier')
 scaler = joblib.load('scaler_svm')
   
@@ -18,22 +19,16 @@ commands = [  '',
               'pyautogui.keyUp("w")',
               'pyautogui.click(button="right")',
               'pyautogui.keyDown("w")',
-              'pyautogui.drag(-150, 0, 0.7, button="left")', 
-              'pyautogui.drag(150, 0, 0.7, button="left")' 
-              
+              'pyautogui.drag(-170, 0, 0.5,button="left")', 
+              'pyautogui.drag(170, 0, 0.5,button="left")' 
            ]
 
+temp = 1
 
-with open('orientations.csv' , 'a' , newline = '') as f :
-   
-    temp = 1
+while True: 
 
-    while True: 
-
-        
-
-        # buffer size is 1024 bytes 
-        data, addr = sock.recvfrom(1024)  
+        buffer_size = 1024  
+        data, addr = sock.recvfrom(buffer_size)  
         val = binascii.hexlify(data).decode('utf-8')
         val = [val[i:i+2] for i in range(0, len(val), 2)]
         measures = val[36:48]
@@ -53,10 +48,7 @@ with open('orientations.csv' , 'a' , newline = '') as f :
         
         if abs(m - temp) !=0 :
              
-
              #pyautogui.click(button='right')
-             
-                
              try :
                 if m == 2 and temp == 3:
                     pyautogui.keyUp("w")
@@ -67,27 +59,21 @@ with open('orientations.csv' , 'a' , newline = '') as f :
                 if m == 1 and temp == 2 :
                     pyautogui.click(button="right")
 
-                
-                        
-                        
                 #exec(commands[m])
+
              except: c__ = 0
              if m in (4,5) and temp == 3:
-                pyautogui.keyUp("w")
-                pyautogui.mouseDown(button='left')
-                pyautogui.drag(-150, 0, 0.7)
-                pyautogui.mouseUp(button='left')
+                #pyautogui.keyUp("w")
+                #pyautogui.keyDown("z")
+                exec(commands[m])
+                #pyautogui.keyUp("z")
+
              else:
                 exec(commands[m])   
 
              if m in (4 , 5):
                     pyautogui.moveTo(950 , 570)
-                    
+           
              
-             
-             
-
-
- 
         temp = m
         #print(m)                                     
