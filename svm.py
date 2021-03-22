e7c916487232cd0c91a72bcaeeed3d7cbf43fdaf
Yash Indane <yashindane46@gmail.com>
2021-03-22 18:53:35 +0530

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,35 @@
+# Random Forest Classification
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import joblib
+
+# Importing the dataset
+dataset = pd.read_csv('orientations.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.05, random_state = 42)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+joblib.dump(sc , 'scaler_svm')
+
+# Training the SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+joblib.dump(classifier , 'svm_classifier')
+
+y_pred = classifier.predict(X_test)
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
